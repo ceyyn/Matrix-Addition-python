249de227f2c2720b8c8bf0ac6bea8b660e434cf4
ceyyn <92554571+ceyyn@users.noreply.github.com>
2021-11-02 19:26:26 +0300

    Add files via upload

---
+++ workshop2.py
@@ -0,0 +1,86 @@
+#first square
+line11 = int(input("Enter the first value of line 1"))
+line12 = int(input("Enter the second value of line 1"))
+line13 = int(input("Enter the third value of line 1"))
+print("\n")
+
+line21 = int(input("Enter the first value of line 2"))
+line22 = int(input("Enter the second value of line 2"))
+line23 = int(input("Enter the third value of line 2"))
+print("\n")
+
+line31 = int(input("Enter the first value of line 3"))
+line32 = int(input("Enter the second value of line 3"))
+line33 = int(input("Enter the third value of line 3"))
+print("\n")
+
+
+
+#second square
+print("\n")
+line211 = int(input("Enter the first value of line 1"))
+line212 = int(input("Enter the second value of line 1"))
+line213 = int(input("Enter the third value of line 1"))
+print("\n")
+
+line221 = int(input("Enter the first value of line 2"))
+line222 = int(input("Enter the second value of line 2"))
+line223 = int(input("Enter the third value of line 2"))
+print("\n")
+
+line231 = int(input("Enter the first value of line 3"))
+line232 = int(input("Enter the second value of line 3"))
+line233 = int(input("Enter the third value of line 3"))
+print("\n")
+
+
+
+#results
+line1 = line11 + line211
+line2 = line12 + line212
+line3 = line13 + line213
+
+line10 = line21 + line221
+line20 = line22 + line222
+line30 = line23 + line223
+
+line100 = line31 + line231
+line200 = line32 + line232
+line300 = line33 + line233
+
+
+
+print("\n")
+
+print(str(line1) + " " + str(line2) + " " + str(line3))
+print(str(line10) + " " + str(line20) + " " + str(line30))
+print(str(line100) + " " + str(line200) + " " + str(line300))
+#başarılı
+
+
+#engin demiroğ'un kodu
+
+# x = [[1,2,5],
+# [3,5,2],
+# [4,2,1]]
+# y = [[2,5,3],
+# [2,4,1],
+# [1,3,2]]
+
+# sonuc = [[0,0,0],
+# [0,0,0],
+# [0,0,0]]
+
+# sonuc[0][0] = x[0][0] + y[0][0]
+# sonuc[0][1] = x[0][1] + y[0][1]
+# sonuc[0][2] = x[0][2] + y[0][2]
+
+# sonuc[1][0] = x[1][0] + y[1][0]
+# sonuc[1][1] = x[1][1] + y[1][1]
+# sonuc[1][2] = x[1][2] + y[1][2]
+
+# sonuc[2][0] = x[2][0] + y[2][0]
+# sonuc[2][1] = x[2][1] + y[2][1]
+# sonuc[2][2] = x[2][2] + y[2][2]
+
+# print(sonuc)
